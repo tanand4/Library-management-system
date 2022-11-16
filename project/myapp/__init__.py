c27afeefa17dcb032f652e47405c019e304bd2fd
Tanmay anand <tanmay@Tanmay_PC.localdomain>
2022-11-16 16:19:14 -0600

    Added PGSQL Connection and test queries

--- project/myapp/__init__.py
+++ project/myapp/__init__.py
@@ -1,1 +1,23 @@
-__version__ = (0, 0, 1)
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="Library", user='postgres', password='tanmay', host='127.0.0.1', port= '5432'
+)
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Executing an MYSQL function using the execute() method
+cursor.execute("select version()")
+
+sql = '''insert into public.\"Users\" values('1','Test','passwd')
+ '''
+  
+cursor.execute(sql)
+# Fetch a single row using fetchone() method.
+cursor.execute("select * from public.\"Users\"")
+data = cursor.fetchall()
+print("Data: ",data)
+conn.commit()
+#Closing the connection
+conn.close()
